d0a22fe0c3acffd67923a5281391e42cf222cb04
Stefan <stefandaniels24@gmail.com>
2022-01-13 14:40:15 +0100

    functions

--- Py/IFC2x3_XML_to_JSON.py
+++ Py/IFC2x3_XML_to_JSON.py
@@ -1,8 +1,35 @@
-from bs4 import BeautifulSoup
+import xmltodict
 import json
 
-for filename in os.listdir("../Source/IFC2x3"):
-    if filename.startswith("Pset_"):
-        print(filename)
-    # xml_parser = BeautifulSoup(ope)
+
+def Parser():
+    with open('../Source/IFC2x3/psd/Pset_ActionRequest.xml', 'r') as myfile:
+        obj = xmltodict.parse(myfile.read())
+    print(json.dumps(obj))
+    return obj
+
+
+def Json(obj):
+    classname = obj["PropertySetDef"]["ApplicableClasses"]["ClassName"][0]
+
+    for url in obj["PropertySetDef"]["ApplicableClasses"]["urls"]:
+        if url["key"] == "MOBILE":
+
+
+
+# Pset = {
+#     classname: [
+#         {
+#             "name": name
+#             "Properties": [
+#                 {
+#                     "name": name,
+#                     "type": "",
+#                 }
+#             ]
+#         }
+#     ]
+# }
+
+
 
