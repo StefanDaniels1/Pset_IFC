3bc5c87fc07af2052a31aeeb8d552297162c3bb6
Stefan <stefandaniels24@gmail.com>
2022-01-14 11:32:05 +0100

    json.dump

--- Py/IFC2x3_XML_to_JSON.py
+++ Py/IFC2x3_XML_to_JSON.py
@@ -1,35 +1,70 @@
 import xmltodict
 import json
+import pandas as pd
 
 
-def Parser():
+def parser():
     with open('../Source/IFC2x3/psd/Pset_ActionRequest.xml', 'r') as myfile:
         obj = xmltodict.parse(myfile.read())
-    print(json.dumps(obj))
+
     return obj
 
+def printt(obj):
+    print(json.dumps(obj))
 
-def Json(obj):
-    classname = obj["PropertySetDef"]["ApplicableClasses"]["ClassName"][0]
+# parser()
 
-    for url in obj["PropertySetDef"]["ApplicableClasses"]["urls"]:
-        if url["key"] == "MOBILE":
+# def pd_json(obj):
+#     df = pd.read_json(obj)
+#     df['PropertySetDef'].applly(pd.Series)
+#     print(df)
+
+
+#
+# def json(obj):
+#     Properties = []
+#     classname = str(obj["PropertySetDef"]["ApplicableClasses"]["ClassName"])
+#     Pset_name = str(obj["PropertySetDef"]["Name"])
+#     for each in obj["PropertySetDef"]["PropertyDefs"]["PropertyDef"]:
+#         Properties.append(str(each["Name"]))
+#     # for i in obj["PropertySetDef"]["PropertyDefs"]["PropertyDef"]:
+#     #     Properties.append(str(i["Name"]))
+#     # dict = {classname: {'name': Pset_name, 'properties': {Properties}}
+#     # json_dump = json.dump(dict)
+#
+#     y = {classname:{"name":[Pset_name]}, "properties":[{"name":Propertie, "type":Type}]}
+#     # print(json.dump(y))
+
+    #
+    #
+    #
+    # {
+    #     classname: [
+    #         {
+    #             "name": Pset_name,
+    #             "properties": [
+    #                 {
+    #                     "name": Propertie,
+    #                     "type": Type
+    #                 },
+    #                 {
+    #                     "name": Propertie,
+    #                     "type": Type
+    #                 }
+    #             ]
+    #         }
+    #     ]
+    # }
+
+printt(parser())
 
 
 
-# Pset = {
-#     classname: [
-#         {
-#             "name": name
-#             "Properties": [
-#                 {
-#                     "name": name,
-#                     "type": "",
-#                 }
-#             ]
-#         }
-#     ]
-# }
+
+
+
+
+
 
 
 
